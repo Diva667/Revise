a889c449de0f2be5cc9dc29575bde9192b3d7bf9
Diva667 <diva667divakar@outlook.com>
2025-09-25 12:24:48 +0530

    Done

---
+++ src/Divide/Palindrome.java
@@ -0,0 +1,15 @@
+package Divide;
+
+public class Palindrome {
+	
+	private void empName() {
+		System.out.println("Test 1");
+
+	}
+	
+	private void empID() {
+		System.out.println("Test 2");
+
+	}
+
+}
